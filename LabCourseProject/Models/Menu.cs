50218ed65b22f0f5c9d2a1c25bdc1cbd905cca05
DoruntinaSh <84545821+DoruntinaSh@users.noreply.github.com>
2021-05-29 17:40:36 +0200

    Krijimi i menyve të restaurantit

---
+++ LabCourseProject/Models/Menu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LabCourseProject.Models
+{
+    public class Menu
+    {
+        [System.ComponentModel.DataAnnotations.Key]
+        public int id { get; set; }
+        public string name { get; set; }
+            
+    }
+}
