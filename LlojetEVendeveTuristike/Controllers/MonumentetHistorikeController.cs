d096c21f5ddbafb44e53bd236b8fe7676297c398
Vanesabunjaku <73537092+Vanesabunjaku@users.noreply.github.com>
2021-06-06 21:37:22 +0200

    Update MonumentetHistorikeController.cs

--- LlojetEVendeveTuristike/Controllers/MonumentetHistorikeController.cs
+++ LlojetEVendeveTuristike/Controllers/MonumentetHistorikeController.cs
@@ -2,13 +2,132 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using LlojetEVendeveTuristike.Models;
 
-namespace LlojetEVendeveTuristike.Models
+namespace LlojetEVendeveTuristike.Controllers
 {
-    public class MonumentetHistorike
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MonumentetHistorikeController : ControllerBase
     {
-        public int MonumentiId { get; set; }
-        public string EmriMonumentit { get; set; }
-        public string EmriLokacionit { get; set; }
+        private readonly IConfiguration _configuration;
+
+        public MonumentetHistorikeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                    select MonumentiId, EmriMonumentit, EmriLokacionit from MonumentetHistorike";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("LlojetEVendeveTuristikeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+
+        [HttpPost]
+        public JsonResult Post(MonumentetHistorike mhs)
+        {
+            string query = @"
+                    insert into MonumentetHistorike values 
+                    ('" + mhs.EmriMonumentit + @"',
+                     '" + mhs.EmriLokacionit + @"')
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("LlojetEVendeveTuristikeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+        }
+
+
+        [HttpPut]
+        public JsonResult Put(MonumentetHistorike mhs)
+        {
+            string query = @"
+                    update MonumentetHistorike set 
+                    EmriMonumentit = '" + mhs.EmriMonumentit + @"'
+                    EmriLokacionit = '" + mhs.EmriLokacionit + @"'
+                    where MonumentiId = " + mhs.MonumentiId + @" 
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("LlojetEVendeveTuristikeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Updated Successfully");
+        }
+
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                    delete from MonumentetHistorike
+                    where MonumentiId = " + id + @" 
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("LlojetEVendeveTuristikeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfully");
+        }
     }
 }
