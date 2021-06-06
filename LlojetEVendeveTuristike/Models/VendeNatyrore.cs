ffd340b61aa86701794a3cfe2bc5c7ca48b35f0a
Vanesabunjaku <73537092+Vanesabunjaku@users.noreply.github.com>
2021-06-06 21:39:02 +0200

    Add files via upload

---
+++ LlojetEVendeveTuristike/Models/VendeNatyrore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LlojetEVendeveTuristike.Models
+{
+    public class VendeNatyrore
+    {
+        public int VendiId { get; set; }
+        public string EmriVendit { get; set; }
+        public string EmriLokacionit { get; set; }
+    }
+}
